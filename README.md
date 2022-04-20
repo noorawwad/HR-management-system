80b50d245d3945b8924aa0dca00079236c599dfe
noorawwad <noorawwad417@gmail.com>
2022-04-20 19:02:48 +0300

    make a style for the home pages as well as answer the task questions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HR-management-system
 Task 7
+
+##What observations or questions do you have about what you’ve learned so far?
+ its was intresting task its open my mind
+
+ ##How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+ i thought its will take the hole day but actually its take 4 hr .
+ 
--- app.js
+++ app.js
@@ -8,15 +8,18 @@ function Employee(employeeID,fullName,department,level,imageURL){
 }
 
 Employee.prototype.salary=function(){
+    let randomSalary;
     let netSalary;
     let taxPersent=0.075;
     if(this.level=="Senior"){
-        netSalary=getRndInteger(1500,2000)-taxPersent*getRndInteger(1500,2000);
+        randomSalary=getRndInteger(1500,2000);
+        netSalary=randomSalary-taxPersent*randomSalary;
     }else if(this.level=="Mid-Senior"){
-        netSalary=getRndInteger(1000,1500)-taxPersent*getRndInteger(1000,1500);
+        randomSalary=getRndInteger(1000,1500)
+        netSalary= randomSalary-taxPersent* randomSalary;
          }
     else {
-        netSalary=getRndInteger(1000,1500)-taxPersent*getRndInteger(500,1000);
+        netSalary=getRndInteger(500,1000)-taxPersent*getRndInteger(500,1000);
       }
 return netSalary;
     }
@@ -25,15 +28,24 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
 
-let employee1= new Employee(1000,"Ghazi Samer","Administration","Senior");
-let employee2= new Employee(1001,"Lana Ali","Finance","Senior");
-let employee3= new Employee(1002,"Tamara Ayoub","Marketing","Senior");
-let employee4= new Employee(1003,"Safi Walid","Administration","Mid-Senior");
-let employee5= new Employee(1004,"Omar Zaid","Development","Senior");
-let employee6= new Employee(1005,"Rana Saleh","Development","Junior");
-let employee7= new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
+let employee1= new Employee(1000,"Ghazi Samer","Administration","Senior","./assets/Ghazi.png");
+let employee2= new Employee(1001,"Lana Ali","Finance","Senior","./assets/lana.png");
+let employee3= new Employee(1002,"Tamara Ayoub","Marketing","Senior","./assets/tamara.png");
+let employee4= new Employee(1003,"Safi Walid","Administration","Mid-Senior","./assets/safi.png");
+let employee5= new Employee(1004,"Omar Zaid","Development","Senior","./assets/omar.png");
+let employee6= new Employee(1005,"Rana Saleh","Development","Junior","./assets/rana.png");
+let employee7= new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","./assets/hadi.png");
 
+// &nbsp is two add a white space && "<br>" to add a line break
 Employee.prototype.render = function () {
-    document.write(`The name of the employee: ${this.fullName} he/she takes :${this.salary()}`);
+    document.write(`The name of the employee: ${this.fullName}  &nbsp &nbsp &nbsp &nbsp   his/her salary :${this.salary()}`);
+    document.write("<br>");
+    
 }
 employee1.render();
+employee2.render();
+employee3.render();
+employee4.render();
+employee5.render();
+employee6.render();
+employee7.render();
